ac544b2b17589e18c0db74f50cbdbdd5c6e32ae6
Pedro Rubio <pedroherub@protonmail.com>
2024-04-07 15:43:29 +0000

    Finish custom filters

--- blango/settings.py
+++ blango/settings.py
@@ -66,7 +66,7 @@ ROOT_URLCONF = 'blango.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],
+        'DIR': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
